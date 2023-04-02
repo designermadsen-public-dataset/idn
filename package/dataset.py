5e9eca4a34b12c54341c8d5114e23571f1c05cf7
Kent Madsen <kent.vejrup.madsen@designermadsen.dk>
2023-04-02 17:52:33 +0200

    making package

---
+++ package/dataset.py
@@ -0,0 +1,12 @@
+from keras.preprocessing.image \
+    import ImageDataGenerator
+
+
+
+def designermadsen_numbers_training_set() -> ImageDataGenerator | None:
+    pass
+
+
+def designermadsen_numbers_validation_set() -> ImageDataGenerator | None:
+    pass
+
